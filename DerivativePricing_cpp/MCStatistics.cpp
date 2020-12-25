112b8369150de4b697adf54223e13d3110edf8ca
RongW_gmail <wangrwill@gmail.com>
2020-12-24 20:43:21 -0500

    remove unused xcode and ds_store files

---
+++ DerivativePricing_cpp/MCStatistics.cpp
@@ -0,0 +1,9 @@
+//
+//  MCStatistics.cpp
+//  DerivativePricing_cpp
+//
+//  Created by Rong Wang on 2020-12-24.
+//  Copyright © 2020 RW. All rights reserved.
+//
+
+#include "MCStatistics.hpp"
---
+++ DerivativePricing_cpp/MCStatistics.hpp
@@ -0,0 +1,14 @@
+//
+//  MCStatistics.hpp
+//  DerivativePricing_cpp
+//
+//  Created by Rong Wang on 2020-12-24.
+//  Copyright © 2020 RW. All rights reserved.
+//
+
+#ifndef MCStatistics_hpp
+#define MCStatistics_hpp
+
+#include <stdio.h>
+
+#endif /* MCStatistics_hpp */
